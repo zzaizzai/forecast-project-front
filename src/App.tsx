3928801b2915935076128a889ca4a98af0a67f85
zzaizzai <junsai0530@gmail.com>
2025-01-02 12:17:23 +0900

    init test

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Test from './components/Test'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -21,6 +22,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <Test />
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
---
+++ src/components/Test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Test: React.FC = () => {
+    return (
+        <div>
+            <h1>Test Component</h1>
+        </div>
+    );
+};
+
+export default Test;
